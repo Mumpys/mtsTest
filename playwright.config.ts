28c72c060e01ee7c0bc76ebeb380b34daa1dd5ab
Mumpys <87718523+Mumpys@users.noreply.github.com>
2024-04-15 09:54:34 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         channel: 'chrome',
         userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
       },
     },
   ],
